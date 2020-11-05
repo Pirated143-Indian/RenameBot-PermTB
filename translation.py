2a2e2448463f2ee838c36e345f7231b8c39d2586
The Bot Gram <71497650+rocky572@users.noreply.github.com>
2020-11-05 12:12:23 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,30 @@
+class Translation(object):
+    START_TEXT = """ <b>A Simple File Renamer Bot With Permanent Thumbnail Support 💯</b>
+
+<b>Send me any Telegram file and reply to that file with */rename* NewName.extn"</b>""
+
+<b>Spam = Direct Ban🚫 From Our All Bots</b>
+
+    RENAME_403_ERR = "*What Are You Doing? You are Banned*"
+    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    UPGRADE_TEXT = "<b>Hey I Puted This Command Simply Upgrade Service Is Not Available🙃 so sed</b>"
+    DOWNLOAD_START = "<b>➶➶➶➶➶ I Am Downloading Your Files To Our Server So Wait 😉</b>
+    UPLOAD_START = "Hey!! Man Wt R U Seeing Your Download Got Completed🎉 Uploading Your File  
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thanks For Using Me (◔‿◔) **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT @Pirated143"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @Pirated143"
+    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Saved For Permanent ✔ 
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Your Thumbnail cleared succesfully🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    CUSTOM_CAPTION_UL_FILE = "Hey This Is Your File Enjoy Share And Support Us @Bot_Gram_Developers"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail📎 found Sed,"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = ""Hey Nothing To See Here Dont Complicate Simply Rename Ur Files Support @Bot_Gram_Developers""
+    
+1. <b>Send me any Telegram File.
+2. Reply to that message with /rename NEWNAME.EXTENSION"""</b>
+
+
