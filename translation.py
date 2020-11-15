61002875c6f5d4c0b33d9eef709a79a7a8c136f9
The Bot Gram <71497650+rocky572@users.noreply.github.com>
2020-11-15 15:11:51 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,36 @@
+class Translation(object):
+    START_TEXT = """ <b>Hey 👋 I Am A Rename Bot 😉
+
+✳️ Maintained By :-  @Bot_Gram_Developers
+
+✳️ Bot By :- @Pirated143
+
+✅ Use /help To Know My Features</b>"""
+
+    RENAME_403_ERR = "*What Are You Doing? You are Banned*"
+    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    UPGRADE_TEXT = "<b>ᴜᴘɢʀᴀᴅᴇ sᴇʀᴠɪᴄᴇ ɪs ɴᴏᴛ ᴀᴠᴀɪʟᴀʙʟᴇ ᴀᴛ ᴛʜɪs ᴍᴏᴍᴇɴᴛ </b>"
+    DOWNLOAD_START = "<b>ᴡᴀɪᴛ !! ɪ ᴀᴍ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ғɪʟᴇ ᴛᴏ ᴍʏ sᴇʀᴠᴇʀ 😉</b>"
+    UPLOAD_START = "<b>ʜᴇʏ 👋 !! ʏᴏᴜʀ ғɪʟᴇ ɢᴏᴛ ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ɴᴏᴡ ᴜᴘʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ғɪʟᴇ</b>" 
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**✳️Thanks For Using Me✳️**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT @Pirated143"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact💁Bot Owner "
+    SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Saved For Permanent ✅</b> "
+    DEL_ETED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Cleared For Permanent✅</b>"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>✅File Downloaded Successfully</b>"
+    CUSTOM_CAPTION_UL_FILE = "<b>✳️ Share And Support Us ✳️</b>"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "<b>❌ No Thumbnail Found</b>"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """<b>🥎There are Some things I can do:
+
+
+✳️ ReName Telegram files, with Permanent thumbnail support
+
+✳️ ReName Files With Many Formats  Like .mkv .mp4 .avi .mp3 .apk EtC 
+
+✅JOIN @Bot_Gram_Developers 😉</b>"""
+
+
